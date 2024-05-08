2268cb55487f9200944908d399a68ed146a9cd09
yunhee0302 <yuuuuuni930302@gmail.com>
2024-05-08 17:52:32 +0900

    변수

---
+++ src/variable/Var1.java
@@ -0,0 +1,11 @@
+package variable;
+
+import java.util.Arrays;
+
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(20); // 변경 10 -> 20
+        System.out.println(20); // 변경 10 -> 20
+        System.out.println(20); // 변경 10 -> 20
+    }
+}
