2ac3c879cad1793120092fff895a86c516afec1a
yunhee0302 <yuuuuuni930302@gmail.com>
2024-05-14 15:06:51 +0900

    스코프, 형변환

---
+++ src/casting/Casting3.java
@@ -0,0 +1,18 @@
+package casting;
+
+public class Casting3 {
+
+    public static void main(String[] args) {
+        long maxIntValue = 2147483647; // int 최고값 (int값을 long에 담음)
+        long maxIntOver = 2147483648L; // int 최고값 + 1(초과)
+        int intValue = 0;
+
+        intValue = (int) maxIntValue;
+        System.out.println("intValue = " + intValue);
+
+        intValue = (int) maxIntOver;
+        System.out.println("intValue = " + intValue);
+
+
+    }
+}
